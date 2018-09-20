d3aa80ca3265f6925debe6242401af91be92243d
a-ogilvie <alex@zehitomo.com>
2018-09-20 11:39:26 +0900

    Collapse duplicate lines and add prefecture array

--- src/reformatData.js
+++ src/reformatData.js
@@ -9,6 +9,7 @@ const reformatted = [];
 data.forEach((prefecture) => {
   prefecture.lines.forEach((line) => {
     // remove zoom
+    line.prefectureIds = prefecture.name.ja;
     delete line.zoom;
     delete line.lat;
     delete line.lng;
@@ -20,24 +21,49 @@ data.forEach((prefecture) => {
       station.lng = station.location.lng;
       station.zipCode = station.location.postalCode.ja.replace(/[^0-9]/g, "");
       delete station.location;
+      const lineIds = [];
       station.lines.forEach((line) => {
-        delete line.name;
+        lineIds.push(line.id);
       });
+      station.lines = lineIds;
     });
     reformatted.push(JSON.parse(JSON.stringify(line)));
   });
 });
 
+const idsToPrefectures = {};
+
 reformatted.forEach((line) => {
-  line.stations.forEach((station, i) => {
-    if (/[^0-9a-zA-ZāōŌūŪ <>()-.'/]/.test(station.name.en)) {
-      console.log(
-        `${station.id} ${station.prefecture.en} ${station.name.ja} ${
-          station.name.en
-        } ${station.lat} ${station.lng}`
-      );
-    }
-  });
+  if (idsToPrefectures.hasOwnProperty(line.id)) {
+    idsToPrefectures[line.id].push(line.prefectureIds);
+  } else {
+    idsToPrefectures[line.id] = [line.prefectureIds];
+  }
 });
 
-writeJson("./data/formatted-data.json", reformatted);
+const duplicates = new Set();
+
+reformatted.forEach((line) => {
+  if (duplicates.has(line.id)) {
+    line.delete = true;
+  } else {
+    duplicates.add(line.id);
+    line.prefectureIds = idsToPrefectures[line.id];
+  }
+});
+
+const deduplicated = reformatted.filter((line) => !line.delete);
+
+writeJson("./data/formatted-data.json", deduplicated);
+
+// reformatted.forEach((line) => {
+//   line.stations.forEach((station, i) => {
+//     if (/[^0-9a-zA-ZāōŌūŪ <>()-.'/]/.test(station.name.en)) {
+//       console.log(
+//         `${station.id} ${station.prefecture.en} ${station.name.ja} ${
+//           station.name.en
+//         } ${station.lat} ${station.lng}`
+//       );
+//     }
+//   });
+// });
